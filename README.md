184df95ce682f29c29e3c4a6488909f3934a80e4
Eden <edendudoo@gmail.com>
2021-10-04 11:24:14 +0800

    Update framework to xcframework.

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 這是我研究 app 透過藍牙進行溝通的測試程式，分為中心與周邊兩個部分，<br/>
 因為這兩部分的設定方式完全不相同，因此將注意事項與說明皆寫在以下的檔案中：<br/>
 
-* [藍牙中心（Central）](https://gitlab.com/darktt/DTBLECommunication/blob/master/DTBLECommunication/Central/README.md)
-* [藍牙周邊（Peripheral）](https://gitlab.com/darktt/DTBLECommunication/blob/master/DTBLECommunication/Peripheral/README.md)
+* [藍牙中心（Central）](DTBLECommunication/blob/master/DTBLECommunication/Central/README.md)
+* [藍牙周邊（Peripheral）](DTBLECommunication/blob/master/DTBLECommunication/Peripheral/README.md)
---
+++ SwiftExtensions.xcframework/ios-arm64_x86_64-simulator/SwiftExtensions.framework/Headers/SwiftExtensions.h
@@ -0,0 +1,17 @@
+//
+//  SwiftExtensions.h
+//  SwiftExtensions
+//
+//  Created by Darktt on 18/5/7.
+//  Copyright © 2018 Darktt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftExtensions.
+FOUNDATION_EXPORT double SwiftExtensionsVersionNumber;
+
+//! Project version string for SwiftExtensions.
+FOUNDATION_EXPORT const unsigned char SwiftExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftExtensions/PublicHeader.h>
